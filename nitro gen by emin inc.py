a34b7001a13df3e0edfcd02b1daf43fa4ae8b9a8
emin999999 <163546443+emin999999@users.noreply.github.com>
2024-03-23 13:50:47 +0300

    Nitro generator and checker by emin inc.py

--- nitro gen by emin inc.py
+++ nitro gen by emin inc.py
@@ -2,6 +2,8 @@ import random
 import string
 import requests
 
+print("Owned By Emin INC.")
+
 def generate_code(length=19):
     characters = string.ascii_letters + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
@@ -23,17 +25,27 @@ def check_code_validity(code):
     else:
         return False
 
+def save_valid_codes(codes):
+    with open("valid_gift_codes.txt", "a") as file:
+        for code in codes:
+            file.write(code + "\n")
+
 def main():
     num_codes = int(input("Üretecek kod miktarını girin: "))
     codes = generate_gift_codes(num_codes)
     
     print("Kodların geçerliliği kontrol ediliyor...")
+    valid_codes = []
     for code in codes:
         if check_code_validity(code):
             print(f"{code} - Geçerli")
+            valid_codes.append(code)
         else:
             print(f"{code} - Geçersiz")
+    
+    if valid_codes:
+        save_valid_codes(valid_codes)
+        print("Geçerli kodlar 'valid_gift_codes.txt' dosyasına kaydedildi.")
 
 if __name__ == "__main__":
     main()
-
